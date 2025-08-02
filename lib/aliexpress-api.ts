8390a3539205c1bdd6ba3c5e83457ef95eda41c9
autodrop <150408242+Samikaiwn@users.noreply.github.com>
2025-08-02 04:02:36 -0500

    Update aliexpress-api.ts

--- lib/aliexpress-api.ts
+++ lib/aliexpress-api.ts
@@ -5,10 +5,13 @@ const BASE_URL = "https://ali-express1.p.rapidapi.com";
 const RAPID_API_KEY = process.env.ALIREXPRESS_API_KEY!;
 const RAPID_API_HOST = "ali-express1.p.rapidapi.com";
 
-export const aliexpressAPI = axios.create({
+// ✅
+const aliexpressAPI = axios.create({
   baseURL: BASE_URL,
   headers: {
     "X-RapidAPI-Key": RAPID_API_KEY,
     "X-RapidAPI-Host": RAPID_API_HOST,
   },
 });
+
+export default aliexpressAPI;
